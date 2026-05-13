451761bc51db797cdd17fcfb400e0ffeaf245254
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-05-13 15:22:47 +0600

    first commit

---
+++ src/app/about/designers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DesignersPage = () => {
+    return (
+        <div>
+            <h2 className='text-4xl text-purple-400'>This is Designers Page</h2>
+        </div>
+    );
+};
+
+export default DesignersPage;
